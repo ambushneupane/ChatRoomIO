4fa8c292a9f7b3bfac62476d887d0af5d61f61cb
ambush <iamherovirat@gmail.com>
2025-09-01 12:18:06 +0545

    refactor: Removed unnecessary code

--- socket/index.js
+++ socket/index.js
@@ -38,58 +38,16 @@ io.use((socket,next)=>{
 })
 
 
-// const handleNickname=(socket,nickname)=>{
-    
-//         nickname= nickname?.trim()||socket.user.username||'Guest'
-//         onlineUsers[socket.id]=nickname
-        
-// }
 
 const handleNickname=(socket,nickname)=>{
     const userId=socket.user.userId;
-    console.log('Userid is ',userId);
     const username= nickname?.trim() ||socket.user.username?.trim()||'Guest';
-    console.log('handleNickname username is ',username)
-    
+  
     if(!onlineUsers[userId]) onlineUsers[userId]={username,sockets:new Set()};
     onlineUsers[userId].sockets.add(socket.id);
-    console.log('fromHandleNickname',onlineUsers)
 
 }
 
-// const handleJoinRoom=(socket,roomName)=>{
-//        if(!validRooms.has(roomName.toLowerCase())){
-//         socket.emit('errorMsg',`Invalid room. Allowed Rooms are ${[...validRooms].join(", ")}`)
-//        return;
-//     }
-//     if(socket.currentRoom){
-//         socket.leave(socket.currentRoom);
-//         if(roomUsers[socket.currentRoom]){
-//             delete roomUsers[socket.currentRoom][socket.id];
-//             io.to(socket.currentRoom).emit('onlineUsers',Object.values(roomUsers[socket.currentRoom]))
-//         }
-//     }
-//     socket.join(roomName);
-//     socket.currentRoom=roomName;
-
-//     if(!roomUsers[roomName]) roomUsers[roomName]={};
-//     roomUsers[roomName][socket.id]=onlineUsers[socket.id]||'GUEST'
-
-//     io.to(roomName).emit('onlineUsers',Object.values(roomUsers[roomName]));
-
-//     const joinMsg = formatMessage('System', `${onlineUsers[socket.id]} has joined the chat`, true);
-
-//     if (!roomMessages[roomName]) roomMessages[roomName] = [];
-
-//     roomMessages[roomName].push(joinMsg);
-
-
-//     socket.emit('chatHistory',roomMessages[roomName]);
-
-//     socket.broadcast.to(roomName).emit('chatMessage',joinMsg);
-    
-
-// }
 const handleJoinRoom = (socket, roomName) => {
     roomName = roomName.toLowerCase();
 
@@ -108,7 +66,6 @@ const handleJoinRoom = (socket, roomName) => {
             const userEntry = roomUsers[oldRoom][socket.user.userId];
             userEntry.sockets.delete(socket.id);
 
-            // Remove user from room if no sockets left
             if (userEntry.sockets.size === 0) {
                 delete roomUsers[oldRoom][socket.user.userId];
                 const leaveMsg = formatMessage('System', `${userEntry.username} has left the chat`, true);
@@ -118,7 +75,6 @@ const handleJoinRoom = (socket, roomName) => {
         }
     }
 
-    //  Join new room
     socket.join(roomName);
     socket.currentRoom = roomName;
 
@@ -132,11 +88,10 @@ const handleJoinRoom = (socket, roomName) => {
 
     roomUsers[roomName][socket.user.userId].sockets.add(socket.id);
 
-    //  Send online users list
-
+   
     io.to(roomName).emit('onlineUsers', Object.values(roomUsers[roomName]).map(u => u.username));
 
-    //  Send system "joined" message only if first tab joins
+   
     if (roomUsers[roomName][socket.user.userId].sockets.size === 1) {
         const username = onlineUsers[socket.user.userId].username;
         const joinMsg = formatMessage('System', `${username} has joined the chat`, true);
@@ -173,26 +128,6 @@ const handleTyping=(socket)=>{
 const handleStopTyping=(socket)=>{
     if(socket.currentRoom) socket.broadcast.to(socket.currentRoom).emit('stopTyping',onlineUsers[socket.id]);
 }
-
-// const handleDisconnect=(socket)=>{
-//     const userId=socket.user.userId
-//     const username= onlineUsers[socket.id] || 'Guest'
-//     delete onlineUsers[socket.id];
-
-//     if(socket.currentRoom && roomUsers[socket.currentRoom]){
-//         delete roomUsers[socket.currentRoom][socket.id]
-      
-//         io.to(socket.currentRoom).emit('onlineUsers',Object.values(roomUsers[socket.currentRoom]));
-//         const leaveMsg= formatMessage(username,`${username} has left the chat ...`,true);
-//         io.to(socket.currentRoom).emit('chatMessage',leaveMsg)
-//     }
-//     if (socket.currentRoom && roomUsers[socket.currentRoom] && Object.keys(roomUsers[socket.currentRoom]).length === 0) {
-//         delete roomMessages[socket.currentRoom];
-//         delete roomUsers[socket.currentRoom];
-//     }
-
-
-// }
 
 
 const handleDisconnect= (socket)=>{
